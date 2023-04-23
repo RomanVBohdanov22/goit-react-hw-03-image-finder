0ac90d16d14ba0b8bd3acc33b69e6bd0ea24ccd8
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-04-23 14:43:11 +0300

    styles prep

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      goit-react-hw-03-image-finder
-    </div>
-  );
-};
+import React, { Component } from 'react';
+//import axios from 'axios';
+/*
+const appStylesOld = {
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: '24px',
+  gap: '12px',
+  color: '#010101',
+};*/
+
+const appStyles = {
+  display: 'grid',
+  gridTemplateColumns: '1fr',
+  gridGap: '16px',
+  paddingBottom: '24px',
+}
+
+
+export class App extends Component {
+
+  render() {
+    return (
+      <div
+        style={{ ...appStyles }}
+      >
+        goit-react-hw-03-image-finder
+      </div>
+    );
+  }
+}
