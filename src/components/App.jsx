539c18ff99d0f3ace6292086ed1a1d6fbba7d193
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-04-23 21:04:05 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,53 +24,70 @@ export class App extends Component {
     error: '',
   };
 
-async dataToState(query, page = 1) { 
+  async dataToState(query, page = 1, update = false, prevState = {}) { 
 
     this.setState({ isLoading: true });
     try {
       const data = await ImageService.getImages(query, page); //"flower", 2
-      console.log("here data", data);
+      //console.log("here data", data);
       const { hits, total, totalHits } = data;
-      
+      if (!update) {
+        this.setState({
+          photos: [...hits],          
+        });
+      }
+      else { 
+        console.log("Must be update");
+        this.setState({
+          photos: [...prevState.photos, ...hits],
+          showLoadMore: page < Math.ceil(totalHits / 12)
+        });
+      }
       this.setState({
-        photos: [...hits],
         total: total,
         totalPhotos: totalHits,
       });
       console.log(this.state.photos);
     } catch (error) {
-      this.setState({ error });
+      this.setState({ error: error.message });
     } finally {
       this.setState({ isLoading: false });
     }
     
   }
 
-  async componentDidMount() { 
-    await this.dataToState("flower", 2);
+ /*async componentDidMount() { 
+    await this.dataToState("flower", 2, false);
 
-  }
-  componentDidUpdate(prevProps, prevState) {
-      
-    if (
-      prevState.query !== this.state.query ||
-      prevState.page !== this.state.page
-    ) {
+  }*/
+
+  async componentDidUpdate(prevProps, prevState) {
+    
+    const {query,  page} = this.state;
+    const update = false;
+    if ( prevState.query !== query || prevState.page !== page)
+    {
       this.setState({ isLoading: true });
-      ImageService.getImages(this.state.query, this.state.page)
+      update = true;
+      await this.dataToState(query, page, update, prevState);
+      {/*
+      ImageService.getImages(query, page)
         .then(data => {
           if (!data.hits.length) {
             this.setState({ isEmpty: true });
             return;
           }
+
           this.setState(prevState => ({
             photos: [...prevState.photos, ...data.photos],
 
             showLoadMore: this.state.page < Math.ceil(data.total_results / 12),
           }));
+
         })
         .catch(error => this.setState({ error: error.message }))
         .finally(this.setState({ isLoading: false }));
+      */}
     }
   }
 
