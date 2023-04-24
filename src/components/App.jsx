63c1a3e24c19b0d538ccbb38a2791f619c0b8bf5
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-04-24 16:21:34 +0300

    need to submit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -61,6 +61,7 @@ export class App extends Component {
       });
       Notiflix.Notify.success(`(12) from ${totalHits} photos at query "${query}"`);
     } catch (error) {
+      Notiflix.Notify.failure(error.message);
       this.setState({ error: error.message });
     } finally {
       this.setState({ isLoading: false });
